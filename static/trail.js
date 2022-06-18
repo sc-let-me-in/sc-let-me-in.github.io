00a3240f31f16db1ec9c9a623e6ad9f4eef78d42
Luke Novak <contact@nluken.com>
2022-06-18 17:21:49 -0400

    initial commit

---
+++ static/trail.js
@@ -0,0 +1,16 @@
+var lastPooped = 0
+var mouseDown = false;
+
+window.addEventListener("mousedown", () => mouseDown = !mouseDown)
+window.addEventListener("mouseup",() => mouseDown = !mouseDown)
+
+window.addEventListener("mousemove", function(e) {
+    if (Date.now() - lastPooped > 10 && mouseDown) {
+        var newDiv = document.createElement("div")
+        newDiv.classList.add("poo-trail")
+        newDiv.style.left = e.clientX - 67 + "px"
+        newDiv.style.top = e.clientY - 67 + "px"
+        document.body.appendChild(newDiv)
+        lastPooped = Date.now()
+    }
+})
